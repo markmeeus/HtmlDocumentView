c80bac26fd61d1194d29719d1a31ef7bbd2e4af7
Mark Meeus <mark.meeus@gmail.com>
2011-05-04 20:22:43 +0200

    Initial Commit

---
+++ HtmlDocumentView/HtmlDocumentViewAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HtmlDocumentViewAppDelegate.h
+//  HtmlDocumentView
+//
+//  Created by Mark Meeus on 04/05/11.
+//  Copyright 2011 Protime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HtmlDocumentViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
